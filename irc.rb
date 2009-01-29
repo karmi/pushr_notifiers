1bc908134cdb5ed412755264d0d1d697640574fd
Karel Minarik <karmi@karmi.cz>
2009-01-29 16:11:15 +0100

    Implemented logic for IRC Pushr notifier

--- irc.rb
+++ irc.rb
@@ -8,12 +8,25 @@ class Irc < Pushr::Notifier::Base
 
   def initialize(config={})
     @config = config
-    puts "OK, IRC initialized with config #{@config.inspect}"
+    Pushr::Logger::LOGGER.fatal('IRC notifier') { "IRC not configured" } unless configured?
   end
 
-  def deliver!
+  def deliver!(notification)
+    return unless configured?
+    message = if notification.success
+      "Deployed #{notification.application} with revision #{notification.repository.info.revision} — #{notification.repository.info.message}"
+        else
+      "FAIL! Deploying #{notification.application} failed. Check log for details."
+    end
     ShoutBot.shout(config['uri'], :as => "Pushr") do |channel|
-      channel.say "Testing...."
+      channel.say message
     end
   end
+
+  private
+
+  def configured?
+    !config['uri'].nil?
+  end
+
 end
