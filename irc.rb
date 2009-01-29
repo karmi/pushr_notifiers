e461d68618609854725660a55894ce2e0a91f938
Karel Minarik <karmi@karmi.cz>
2009-01-29 18:40:33 +0100

    Remove un-needed logic -- now in Pushr::Notifier::Base

--- irc.rb
+++ irc.rb
@@ -4,23 +4,9 @@ require 'shout-bot' # $ sudo gem install sr-shout-bot --source=http://gems.githu
 # = IRC notifications for Pushr (http://github.com/karmi/pushr/)
 class Irc < Pushr::Notifier::Base
 
-  attr_reader :config
-
-  def initialize(config={})
-    @config = config
-    Pushr::Logger::LOGGER.fatal('IRC notifier') { "IRC not configured" } unless configured?
-  end
-
   def deliver!(notification)
     return unless configured?
-    message = if notification.success
-      "Deployed #{notification.application} with revision #{notification.repository.info.revision} — #{notification.repository.info.message}"
-        else
-      "FAIL! Deploying #{notification.application} failed. Check log for details."
-    end
-    ShoutBot.shout(config['uri'], :as => "Pushr") do |channel|
-      channel.say message
-    end
+    ShoutBot.shout(config['uri'], :as => "Pushr") { |channel| channel.say message(notification) }
   end
 
   private
