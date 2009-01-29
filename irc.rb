f35981a735a639a6237dbf315f83102091826c96
Karel Minarik <karmi@karmi.cz>
2009-01-29 14:53:56 +0100

    Just testing stuff....

--- irc.rb
+++ irc.rb
@@ -1,3 +1,19 @@
+require 'rubygems'
+require 'shout-bot' # $ sudo gem install sr-shout-bot --source=http://gems.github.com
+
 # = IRC notifications for Pushr (http://github.com/karmi/pushr/)
-class IRC < Pushr::Notifier::Base
+class Irc < Pushr::Notifier::Base
+
+  attr_reader :config
+
+  def initialize(config={})
+    @config = config
+    puts "OK, IRC initialized with config #{@config.inspect}"
+  end
+
+  def deliver!
+    ShoutBot.shout(config['uri'], :as => "Pushr") do |channel|
+      channel.say "Testing...."
+    end
+  end
 end
